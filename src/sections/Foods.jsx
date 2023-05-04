e3b281884387add14342444240bd19c6c4690a7b
v-Alqorith <vahapcandongel@hotmail.com>
2023-05-04 21:13:06 +0100

    animated pagination

--- src/sections/Foods.jsx
+++ src/sections/Foods.jsx
@@ -11,7 +11,7 @@ export default function Foods() {
     const { page } = usePageStore();
 
     return (
-        <div className="my-20 flex justify-between flex-col items-center w-[80%] sm:flex-col gap-4 sm:w-full" id="foods">
+        <div className="my-20 flex justify-between flex-col items-center w-[80%]  sm:flex-col gap-4 sm:w-full" id="foods">
             <SectionTitle title={'Foods'} />
             <CategoryNav />
 
@@ -26,6 +26,31 @@ export default function Foods() {
                         </div>
                     </CSSTransition>
                 )}
+
+                {page === "sushi" && (
+                    <CSSTransition timeout={500} classNames="slide">
+                        <div className='overflow-auto grid grid-cols-2 gap-8 rounded-md w-1/2 sm:grid-cols-1 sm:w-full sm:flex sm:flex-col sm:justify-center sm:items-center'>
+                            Sushi
+                        </div>
+                    </CSSTransition>
+                )}
+                {page === "drink" && (
+                    <CSSTransition timeout={500} classNames="slide">
+                        <div className='overflow-auto grid grid-cols-2 gap-8 rounded-md w-1/2 sm:grid-cols-1 sm:w-full sm:flex sm:flex-col sm:justify-center sm:items-center'>
+                            Drink
+                        </div>
+                    </CSSTransition>
+                )}
+                {page === "dessert" && (
+                    <CSSTransition timeout={500} classNames="slide">
+                        <div className='overflow-auto grid grid-cols-2 gap-8 rounded-md w-1/2 sm:grid-cols-1 sm:w-full sm:flex sm:flex-col sm:justify-center sm:items-center'>
+                            Dessert
+                        </div>
+                    </CSSTransition>
+                )}
+
+
+
             </TransitionGroup>
         </div>
     );
