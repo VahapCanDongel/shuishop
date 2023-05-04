ec0828c02b1189a10d1b68dc5248c09730286e1e
v-Alqorith <vahapcandongel@hotmail.com>
2023-05-04 21:36:23 +0100

    removed spacing

--- src/sections/Foods.jsx
+++ src/sections/Foods.jsx
@@ -15,36 +15,28 @@ export default function Foods() {
             <SectionTitle title={'Foods'} />
             <CategoryNav />
             {page === "noodle" && (
-
                 <div className=' overflow-auto grid grid-cols-2 gap-8 rounded-md w-1/2 sm:grid-cols-1 sm:w-full sm:flex sm:flex-col sm:justify-center sm:items-center'>
                     <Card />
                     <Card />
                     <Card />
                     <Card />
                 </div>
-
             )}
 
             {page === "sushi" && (
-
                 <div className='overflow-auto grid grid-cols-2 gap-8 rounded-md w-1/2 sm:grid-cols-1 sm:w-full sm:flex sm:flex-col sm:justify-center sm:items-center'>
                     Sushi
                 </div>
-
             )}
             {page === "drink" && (
-
                 <div className='overflow-auto grid grid-cols-2 gap-8 rounded-md w-1/2 sm:grid-cols-1 sm:w-full sm:flex sm:flex-col sm:justify-center sm:items-center'>
                     Drink
                 </div>
-
             )}
             {page === "dessert" && (
-
                 <div className='overflow-auto grid grid-cols-2 gap-8 rounded-md w-1/2 sm:grid-cols-1 sm:w-full sm:flex sm:flex-col sm:justify-center sm:items-center'>
                     Dessert
                 </div>
-
             )}
 
         </div>
