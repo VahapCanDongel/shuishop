7b1d2938c756c0c142fe1ccf6387a959cfa6fb12
v-Alqorith <vahapcandongel@hotmail.com>
2023-05-04 21:34:05 +0100

    removed animation

--- src/sections/Foods.jsx
+++ src/sections/Foods.jsx
@@ -11,47 +11,42 @@ export default function Foods() {
     const { page } = usePageStore();
 
     return (
-        <div className="my-20 flex justify-between flex-col items-center w-[80%]  sm:flex-col gap-4 sm:w-full" id="foods">
+        <div className="my-20 flex justify-between flex-col items-center w-[80%]  sm:flex-col gap-4 sm:w-full transition" id="foods">
             <SectionTitle title={'Foods'} />
             <CategoryNav />
+            {page === "noodle" && (
 
-            <TransitionGroup component={null}>
-                {page === "noodle" && (
-                    <CSSTransition timeout={500} classNames="slide">
-                        <div className='overflow-auto grid grid-cols-2 gap-8 rounded-md w-1/2 sm:grid-cols-1 sm:w-full sm:flex sm:flex-col sm:justify-center sm:items-center'>
-                            <Card />
-                            <Card />
-                            <Card />
-                            <Card />
-                        </div>
-                    </CSSTransition>
-                )}
+                <div className=' overflow-auto grid grid-cols-2 gap-8 rounded-md w-1/2 sm:grid-cols-1 sm:w-full sm:flex sm:flex-col sm:justify-center sm:items-center'>
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                </div>
 
-                {page === "sushi" && (
-                    <CSSTransition timeout={500} classNames="slide">
-                        <div className='overflow-auto grid grid-cols-2 gap-8 rounded-md w-1/2 sm:grid-cols-1 sm:w-full sm:flex sm:flex-col sm:justify-center sm:items-center'>
-                            Sushi
-                        </div>
-                    </CSSTransition>
-                )}
-                {page === "drink" && (
-                    <CSSTransition timeout={500} classNames="slide">
-                        <div className='overflow-auto grid grid-cols-2 gap-8 rounded-md w-1/2 sm:grid-cols-1 sm:w-full sm:flex sm:flex-col sm:justify-center sm:items-center'>
-                            Drink
-                        </div>
-                    </CSSTransition>
-                )}
-                {page === "dessert" && (
-                    <CSSTransition timeout={500} classNames="slide">
-                        <div className='overflow-auto grid grid-cols-2 gap-8 rounded-md w-1/2 sm:grid-cols-1 sm:w-full sm:flex sm:flex-col sm:justify-center sm:items-center'>
-                            Dessert
-                        </div>
-                    </CSSTransition>
-                )}
+            )}
 
+            {page === "sushi" && (
 
+                <div className='overflow-auto grid grid-cols-2 gap-8 rounded-md w-1/2 sm:grid-cols-1 sm:w-full sm:flex sm:flex-col sm:justify-center sm:items-center'>
+                    Sushi
+                </div>
 
-            </TransitionGroup>
+            )}
+            {page === "drink" && (
+
+                <div className='overflow-auto grid grid-cols-2 gap-8 rounded-md w-1/2 sm:grid-cols-1 sm:w-full sm:flex sm:flex-col sm:justify-center sm:items-center'>
+                    Drink
+                </div>
+
+            )}
+            {page === "dessert" && (
+
+                <div className='overflow-auto grid grid-cols-2 gap-8 rounded-md w-1/2 sm:grid-cols-1 sm:w-full sm:flex sm:flex-col sm:justify-center sm:items-center'>
+                    Dessert
+                </div>
+
+            )}
+
         </div>
     );
 }
