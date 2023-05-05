bdd84342399cb36830e2cf514d9fb45d85b652fd
VahapCanDongel <vahapcandongel@hotmail.com>
2023-05-05 09:19:24 +0100

    Retrieved data

--- src/components/Specials/Card.jsx
+++ src/components/Specials/Card.jsx
@@ -1,14 +1,14 @@
-export default function Card() {
+export default function Card({imgSrc, title, description, price}) {
     return (
-        <div className="bg-gray-200 w-[300px] h-[400px] rounded-md shadow-lg relative flex flex-col items-center">
+        <div className="bg-gray-200 w-[300px] h-[400px] rounded-md shadow-lg relative flex flex-col items-center flex-shrink-0">
             <div className="">
                 <img className="object-cover" src="https://www.sushido.co.uk/wp-content/uploads/2022/07/sushi-removebg-preview.png" />
             </div>
 
-            <div className="mr-auto font-bold ml-8 text-[20px]">Sushi Pack</div>
+            <div className="mr-auto font-bold ml-8 text-[20px]">{title}</div>
 
-            <p className="ml-8 text-[13px]">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, voluptates!</p>
-            <div className="mr-auto ml-8 mt-10 text-[18px] font-bold">£10.99</div>
+            <p className="ml-8 text-[13px]">{description}</p>
+            <div className="mr-auto ml-8 mt-10 text-[18px] font-bold">£{price}</div>
             <div className="bg-green-400 rounded-xl p-4 w-[140px] flex justify-center items-center absolute bottom-[-25px] left-[70px] cursor-pointer hover:rotate-2 transition">Add To Cart</div>
         </div>
     )
--- src/sections/Specials.jsx
+++ src/sections/Specials.jsx
@@ -1,39 +1,42 @@
-
 import SectionTitle from "../components/Section/SectionTitle";
 import Card from "../components/Specials/Card";
+import { useState, useEffect } from "react";
 
 export default function Specials() {
-    return (
-        <div className="my-20 flex justify-between flex-col items-center w-[80%] sm:flex-col" id="specials">
+  const [offerFoods, setOfferFoods] = useState([]);
 
-            <div className="mr-auto flex justify-between w-full">
-                <SectionTitle title={'Our Special Food Deals %'}/>
-             
+  useEffect(() => {
+    fetch("../../utils/offers.json")
+      .then((response) => response.json())
+      .then((data) => {
+        setOfferFoods(data);
+      })
+      .catch((error) => {
+        console.error("Error retrieving JSON data: ", error);
+      });
+  }, []);
 
-                <div className="flex gap-2">
-                    <div className="w-[40px] h-[40px] bg-pink-200 flex justify-center items-center text-[40px] rounded-md rotate-180 cursor-pointer">
-                        <svg width={46} height={46} className="fill-gray-800" viewBox="0 0 24 24">
-                            <path d="m9.5 17 5-5-5-5v10Z" />
-                        </svg>
-                    </div>
-                    <div className="w-[40px] h-[40px] bg-pink-400 flex justify-center items-center text-[40px] rounded-md cursor-pointer">
-                        <svg width={46} height={46} className="fill-gray-800" viewBox="0 0 24 24">
-                            <path d="m9.5 17 5-5-5-5v10Z" />
-                        </svg>
-                    </div>
-                </div>
+  return (
+    <div
+      className="my-20 flex justify-between flex-col items-center w-[80%] sm:flex-col"
+      id="specials"
+    >
+      <div className="mr-auto flex justify-between w-full">
+        <SectionTitle title={"Our Special Food Deals %"} />
+      </div>
 
-            </div>
-
-
-            <div className="flex gap-4 justify-between items-center w-full  my-4 sm:flex-col sm:gap-14">
-                <Card/>
-                <Card/>
-                <Card/>
-            </div>
-
-
-
-        </div>
-    )
+      <div className="flex gap-24 justify-between items-center w-full  my-4 sm:flex-col sm:gap-14 overflow-x-scroll h-[600px] ">
+        {Array.isArray(offerFoods) &&
+          offerFoods.map((item, index) => (
+            <Card
+              key={index}
+              imgSrc={item.img}
+              title={item.title}
+              description={item.description}
+              price={item.price}
+            />
+          ))}
+      </div>
+    </div>
+  );
 }
